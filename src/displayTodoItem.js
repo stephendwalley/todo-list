e3862e2e0a8d00fb0bf22305452f39a5a965ba04
Stephen Walley <stephendwalley@icloud.com>
2021-06-26 15:09:16 +1000

    Added edit tile DOM functionality

--- src/displayTodoItem.js
+++ src/displayTodoItem.js
@@ -14,35 +14,61 @@ const todoPage = (todo) => {
 
 	spanClose.onclick = () => {
 		detailsModal.remove();
-	}
+	};
 
-	window.onclick = function(event) {
+	window.onclick = function (event) {
 		if (event.target == detailsModal) {
-		  detailsModal.remove();
+			detailsModal.remove();
 		}
-	  }
+	};
 
 	let todoInformation = document.createElement("p");
 
 	detailsModal.appendChild(detailsModalContent);
 	detailsModalContent.append(spanClose, todoInformation);
 
-	let title = todo.title;
-	let description = todo.description;
-	let dueDate = todo.dueDate;
-	let priority = todo.priority;
-	let notes = todo.notes;
+	let title = document.createElement("div");
+	title.innerHTML = todo.title;
+	title.onclick = function () {
+		let editTitleDiv = document.createElement("input");
+		editTitleDiv.type = "text";
+		editTitleDiv.value = todo.title;
 
-	todoInformation.innerHTML += `<ul>
-            <li>${title}</li>
-            <li>${description}</li>
-            <li>${dueDate}</li>
-            <li>${priority}</li>
-            <li>${notes}</li>
-        </ul>`;
+		let saveTitle = document.createElement("button");
+		saveTitle.innerHTML = "Save";
+		saveTitle.onclick = function () {};
+
+		let cancelTitle = document.createElement("button");
+		cancelTitle.innerHTML = "Cancel";
+		cancelTitle.onclick = function () {};
+
+		title.innerHTML = "";
+		title.append(editTitleDiv, saveTitle, cancelTitle);
+	};
+
+	let description = document.createElement("div");
+	description.innerHTML = todo.description;
+
+	let dueDate = document.createElement("div");
+	dueDate.innerHTML = todo.dueDate;
+
+	let priority = document.createElement("div");
+	priority.innerHTML = todo.priority;
+
+	let notes = document.createElement("div");
+	notes.innerHTML = todo.notes;
+
+	todoInformation.append(title, description, dueDate, priority, notes);
+	// todoInformation.innerHTML += `<ul>
+	//         <li>${title}</li>
+	//         <li>${description}</li>
+	//         <li>${dueDate}</li>
+	//         <li>${priority}</li>
+	//         <li>${notes}</li>
+	//     </ul>`;
 
 	content.appendChild(detailsModal);
-	
+
 	return content;
 };
 
